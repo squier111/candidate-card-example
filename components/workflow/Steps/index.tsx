bf4ae46bb23f38ad6b93fd3e89c0a845b0a368d1
Oleg Bereza <ob@proverstka.com>
2023-07-18 15:22:29 +0300

    remove components

--- components/workflow/Steps/index.tsx
+++
@@ -1,6 +0,0 @@
-export { StepGoNoGo } from './StepGoNoGo/StepGoNoGo';
-export { StepInterview } from './StepInterview/StepInterview';
-export { StepAdministrative } from './StepAdministrative/StepAdministrative';
-export { StepSendNotification } from './StepSendNotification/StepSendNotification';
-export { StepOffer } from './StepOffer/StepOffer';
-export { StepAssessment } from './StepAssessment/StepAssessment';
